09bdb78690d545f0bb29f9e22511ad31df35fc7e
mftbusra <135023373+mftbusra@users.noreply.github.com>
2025-01-11 01:41:52 -0500

    description

--- Intro to Python - CS102/Folklore_Hero.py
+++ Intro to Python - CS102/Folklore_Hero.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # Folkore Hero
 # Author: Mst. Fariha Tasnim Busra
-# Date: 09/01/2025
-
-
+# Date: 11/01/2025
+# Description: A text-based adventure game where the player must save their village from a mythical threat. The game includes structured functions, input validation, lists, and win/lose scenarios.
