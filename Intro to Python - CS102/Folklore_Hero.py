af0ad98c8dcd61075899fc2b61651b8147a97ace
mftbusra <mbusra@brynmawr.edu>
2025-01-12 23:20:47 -0500

    change logic padma

--- Intro to Python - CS102/Folklore_Hero.py
+++ Intro to Python - CS102/Folklore_Hero.py
@@ -85,7 +85,10 @@ def padma_river_encounter(inventory, morale):
     if choice == 1:
         print("\nYou can not keep up with the flow of cold water of the Padma. You drown.")
         inventory.append("Boatman")
-        break
+        morale == -1
+        if morale < 0:
+            print("\nYour journey ends here.")
+        return False, morale
     elif choice == 2:
         print("\nThe boatman takes the fish and takes you across.")
         inventory.append("Boatman")
